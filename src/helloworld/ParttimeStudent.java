46b2d788bc18626d05a67c7b9682228a8e3bb9f0
Jainam Patel <pate5295@sheridancollege.ca>
2024-01-24 14:37:52 -0500

    Added class

---
+++ src/helloworld/ParttimeStudent.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author jaina
+ */
+public class ParttimeStudent extends HelloWorld {
+    private int numofCourses;
+
+    public int getNumofCourses() {
+        return numofCourses;
+    }
+
+    public void setNumofCourses(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+    
+    public ParttimeStudent(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+}
